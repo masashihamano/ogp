56c689220fcb8d1b30451789ab1a99086d6cc405
masashihamano <masashi.hamano@gmail.com>
2018-10-16 17:59:05 +0900

    add copy URL system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+
+  # ログインや会員登録をしたら、最後にログインをしようとしたページが表示されるように設定
+  before_action :store_user_location!, if: :storable_location?
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
 end
